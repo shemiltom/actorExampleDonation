835c786e7a9b080824f9fd55d2985ddbe493a679
shemiltomelixr <shemil.tom@theelixrlabs.com>
2024-04-15 14:50:24 +0530

    updated ui

--- Bank/Bank/DonationView.swift
+++ Bank/Bank/DonationView.swift
@@ -8,18 +8,30 @@
 import SwiftUI
 
 struct DonationView: View {
+    
+    /// viewmodel as onbervedobject
     @ObservedObject var viewModel = DonationViewModel()
     var body: some View {
         VStack {
-            Text("Total balance!")
+            Text("Total balance")
+                .bold()
+                .padding()
             Text(viewModel.balance)
-            
+                .font(.title)
+                .bold()
+                .padding()
+            //Button to add deposit
             Button {
                 Task {
-                   await viewModel.receiveDeposits()
+                    await viewModel.receiveDeposits()
                 }
             } label: {
                 Text("donate")
+                    .foregroundColor(.white)
+                    .frame(width: 100, height: 40)
+                    .background(.blue)
+                    .cornerRadius(9.0)
+                
             }
         }
         .padding()
